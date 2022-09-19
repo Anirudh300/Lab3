c1272d9bdddd21f3fdf4882cddbe13ab61c477a1
anirudhbakshi <Anirudh.bakshi30@gmai.com>
2022-09-19 17:11:50 -0400

    Project

---
+++ src/AddressBook.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+    List<BuddyInfo> buddyInfos = new ArrayList<BuddyInfo>();
+
+
+    public void addBuddy(BuddyInfo buddy) {
+        buddyInfos.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddyInfos.remove(buddy);
+    }
+
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        
+
+
+
+
+    }
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,39 @@
+import sun.jvm.hotspot.debugger.Address;
+
+public class BuddyInfo {
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo(){
+        name = "Addy";
+        address = "012";
+        phoneNumber = "3438889999";
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
+    private String address;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private String phoneNumber;
+
+    public static void main(String[] args) {
+        BuddyInfo info = new BuddyInfo();
+
+    }
+}
