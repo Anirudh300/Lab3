4246af25870ea5c6b08a536475ff4cd1ba289dc3
anirudhbakshi <Anirudh.bakshi30@gmai.com>
2022-09-19 17:12:54 -0400

    added remove method in main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,7 +19,7 @@ public class AddressBook {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        
+        addressBook.removeBuddy(buddy);
 
 
 
